01a07298460c91e1586fbce7c0cb16d352a2e1b0
Allreview <60billion@allrvw.com>
2018-09-03 11:48:13 +0900

    control likeReview 2018-09-03 modify3

--- server.js
+++ server.js
@@ -89,6 +89,7 @@ app.post('/wantit',verify,function(req,res){
 						res.send({result:"decrease"})
 						sql1_2 = "select likeReview from user where username = ?"
 						conn.query(sql1_2,username,function(err,rows,field){
+							console.log(typeod(id));
 							var userArray = rows[0];
 							var stringfy1 = ""
 							for(b in userArray){
