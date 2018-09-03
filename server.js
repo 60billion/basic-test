97ac73e8833de1de4b7500c9cdcdacf0953a4acf
Allreview <60billion@allrvw.com>
2018-09-03 11:58:41 +0900

    control likeReview 2018-09-03 modify11

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ app.post('/wantit',verify,function(req,res){
 						conn.query(sql1_2,username,function(err,rows,field){
 							var realId = String(id);
 							var userArray = rows[0];
-							console.log(userArray)
+							console.log(userArray.likeReview)
 							var stringfy1 = ""
 							for(b in userArray){
 								if(userArray[b] != realId || userArray[b] != ""){
