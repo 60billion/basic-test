a4c9cc904a85032bcab5a432b8b42baddf278431
Allreview <60billion@allrvw.com>
2018-09-07 17:00:07 +0900

    comment count2

--- server.js
+++ server.js
@@ -83,6 +83,20 @@ app.post('/getcomments',function(req,res){
 				underComments:rowsUnder
 			}
 			res.send({result:comments})
+			var sql2 = `select reviewId from comment where reviewId = ${reviewId}`
+			var sql3 = `select reviewId from underComment where reviewId = ${reviewId}`
+			var sql4 = `update review set cocount = ? where id = ?`
+			conn.query(sql2,function(err,rows,fields){
+				var count1 = rows.length;
+				conn.query(sql3,function(err,rows,fields){
+					var count2 = rows.length;
+					var total = count1 + count2;
+					var realtotal = String(total)
+					conn.query(sql4,[realtotal,reviewId],function(err,rows,fields){
+
+					})
+				})
+			})
 		})
 	})
 })
