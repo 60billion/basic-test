434e481cb7bf357c5024e5bc3f54faf4d90c2496
Allreview <60billion@allrvw.com>
2018-09-11 11:43:25 +0900

    comment count3

--- server.js
+++ server.js
@@ -138,7 +138,7 @@ app.post('/getcomments',function(req,res){
 		if(rowsTop[0] == null){
 			res.send({noComments:"noComments"})
 			//여기부터는 댓글카운터 업데이트하는 내용
-			var sql2 = `select reviewId from comment where reviewId = ${reviewId} and del =1;`
+			var sql2 = `select reviewId from comment where reviewId = ${reviewId} and  del =1;`
 			var sql3 = `select reviewId from underComment where reviewId = ${reviewId} and del =1;`
 			var sql4 = `update review set cocount = ? where id = ?`
 			conn.query(sql2,function(err,rows,fields){
@@ -481,28 +481,13 @@ app.post('/profileMain',verify,function(req,res){
 })
 
 app.post('/getall',verify,function(req,res){
-	//여기부터는 댓글카운터 업데이트하는 내용
-	var sql2 = `select reviewId from comment where reviewId = ${reviewId} and del =1;`
-	var sql3 = `select reviewId from underComment where reviewId = ${reviewId} and del =1;`
-	var sql4 = `update review set cocount = ? where id = ?`
-	conn.query(sql2,function(err,rows,fields){
-		var count1 = rows.length;
-		conn.query(sql3,function(err,rows,fields){
-			var count2 = rows.length;
-			var total = count1 + count2;
-			var realtotal = String(total)
-			conn.query(sql4,[realtotal,reviewId],function(err,rows,fields){
-				var sql = 'select * from review';
-				conn.query(sql,function(err,rows,fields){
+			var sql = 'select * from review';
+			conn.query(sql,function(err,rows,fields){
 					res.send({
 						reviews:rows
 					})		
 				})
-			})
 		})
-	})
-			
-})
 
 //REATE TABLE `user` (
 //			    `id`  tinyint NOT NULL  AUTO_INCREMENT,
