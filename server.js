e516b8e8988710ed1dc01f3987c5a1cbc44a1b0e
Allreview <60billion@allrvw.com>
2018-09-14 16:46:23 +0900

    short12

--- server.js
+++ server.js
@@ -676,17 +676,6 @@ app.post('/profileMain',verify,function(req,res){
 app.post('/getall',verify,function(req,res){
 	var sql = 'select * from review';
 	var sql1 = `update review set tempReview = short;`
-	//conn.query(sql1,function(err,rows,fields){
-		conn.query(sql,function(err,rows,fields){
-			res.send({
-				reviews:rows
-			})
-		})		
-	//})
-})
-app.post('/getalls',verify,function(req,res){
-	var sql = 'select * from review';
-	var sql1 = `update review set tempReview = short;`
 	conn.query(sql1,function(err,rows,fields){
 		conn.query(sql,function(err,rows,fields){
 			res.send({
@@ -694,6 +683,17 @@ app.post('/getalls',verify,function(req,res){
 			})
 		})		
 	})
+})
+app.post('/getalls',verify,function(req,res){
+	var sql = 'select * from review';
+	var sql1 = `update review set tempReview = short;`
+	//conn.query(sql1,function(err,rows,fields){
+		conn.query(sql,function(err,rows,fields){
+			res.send({
+				reviews:rows
+			})
+		})		
+	//})
 })
 
 app.post('/verifyLogin',verify,function(req,res){
